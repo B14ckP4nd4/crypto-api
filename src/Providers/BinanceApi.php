51b6a3b53c15496291c888132831afed2acddf68
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2022-03-12 15:27:41 +0000

    add get pairs and prices methods

--- src/Providers/BinanceApi.php
+++ src/Providers/BinanceApi.php
@@ -4,33 +4,77 @@ namespace BlackPanda\CryptoApi\Providers;
 
 use Binance\API;
 use BlackPanda\CryptoApi\Contracts\CoinApi;
+use BlackPanda\CryptoApi\Contracts\CoinApiInterface;
+use BlackPanda\CryptoApi\utils\Math;
+use Illuminate\Support\Collection;
 
-class BinanceApi extends CoinApi
+class BinanceApi extends CoinApi implements CoinApiInterface
 {
-    public function __construct($api_key , $api_sec , $api_config)
+    private $BTC_PRICE = null;
+    public function __construct($api_key , $api_sec , $api_config = null)
     {
         if($api_key && $api_sec)
             $this->api = new API($api_key, $api_sec);
 
         if($api_config && file_exists($api_config))
             $this->api = new API($api_config);
-
-        if(!$this->api) throw new \Exception("Api Didn't set properly");
     }
 
-    public function getCoinsList()
+    private function isAPIStatusOK(){
+        if(!$this->api && $this->api->systemStatus()['api']['status'] == 'ping ok')
+            return true;
+    }
+
+    private function coinsList(){
+        $coinsList = [];
+        foreach ($this->api->coins() as $coin){
+            if($coin['isLegalMoney'])
+                continue;
+            $coinsList[$coin['coin']] = [
+                'symbol' => $coin['coin'],
+                'name' => $coin['name'],
+            ];
+        }
+
+        return collect($coinsList);
+    }
+
+    private function getPairs() : Collection {
+        $pairs = [];
+        foreach ($this->api->exchangeInfo()['symbols'] as $pair){
+            $pairs[$pair['symbol']] = [
+              'symbol' => $pair['symbol'],
+              'baseAsset' => $pair['baseAsset'],
+              'quoteAsset' => $pair['quoteAsset'],
+              'baseAssetPrecision' => $pair['baseAssetPrecision'],
+              'quoteAssetPrecision' => $pair['quoteAssetPrecision'],
+            ];
+        }
+
+        return collect($pairs);
+    }
+
+    public function getCoinPriceInUSDT($coin)
     {
-        // TODO: Implement getCoinsList() method.
+        $pair = strtoupper($coin) . "USDT";
+        $price = $this->api->price($pair);
+        if($price)
+        {
+            return $price;
+        }
+
+        return false;
     }
 
-    public function getCoinPrice()
+    public function getCoinPriceInBTC($coin)
     {
-        // TODO: Implement getCoinPrice() method.
-    }
+        $pair = strtoupper($coin) . "BTC";
+        $price = $this->api->price($pair);
+        if($price)
+        {
+            return $price;
+        }
 
-    public function livePrice()
-    {
-        // TODO: Implement livePrice() method.
+        return false;
     }
-
 }
