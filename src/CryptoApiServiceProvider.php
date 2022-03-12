1caa630864d19ef99a20d3be7f9b7b820ea5f1f8
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2022-03-12 11:05:39 +0000

    create a config file and register in publishes

--- src/CryptoApiServiceProvider.php
+++ src/CryptoApiServiceProvider.php
@@ -8,5 +8,11 @@ use Illuminate\Support\ServiceProvider;
 
 class CryptoApiServiceProvider extends ServiceProvider
 {
-
+    public function boot()
+    {
+        /* Css files */
+        $this->publishes([
+            __DIR__ . "/publishes/coinApi.php" => config_path("coinApi.php"),
+        ],"CoinApi-Config");
+    }
 }
---
+++ src/publishes/coinApi.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Binance' =>
+        [
+            'api_key' => env("CRYPTO_API_BINANCE_KEY",null),
+            'api_sec' => env("CRYPTO_API_BINANCE_SEC",null),
+        ]
+];
